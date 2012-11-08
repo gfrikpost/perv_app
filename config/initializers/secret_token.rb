26dbee5ec4ebfcd1fd46f003f0b286381b4b1587
gfrikpost <gfrikpost@gmail.com>
2012-11-08 18:45:18 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PervApp::Application.config.secret_token = '08608422619de886930a40bae37bda35f612b40ee56abc1f5be27e5f450de8c203b80a7d0d5f46afc742b695741a87a5c597c591565921f5ad5216926382e73a'
